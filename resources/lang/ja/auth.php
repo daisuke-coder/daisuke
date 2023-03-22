2aad509e3e562af1017ee3434fd4ea3ca7b486cb
daisuke-coder <125184857+daisuke-coder@users.noreply.github.com>
2023-03-22 19:58:20 +0900

    Add files via upload

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'idかパスワードが一致しません。未登録であれば登録してください。',
+    'throttle' => 'ログインに続けて失敗しています。数秒後に再度お試しください。',
+
+];
