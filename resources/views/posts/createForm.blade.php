d4a7d09a2c3288a97ad0f29ec291a4ff3199880a
daisuke-coder <125184857+daisuke-coder@users.noreply.github.com>
2023-03-23 23:27:10 +0900

    Update createForm.blade.php

--- resources/views/posts/createForm.blade.php
+++ resources/views/posts/createForm.blade.php
@@ -1,20 +1,21 @@
-@extends('layouts.app')
+  @extends('layouts.app')
 
-@section('content')
-<div class="container update-form">
-  <h2 class="page-header">投稿の編集</h2>
-  {!! Form::open(['url'=>'/post/update']) !!}
-  <div class="form-group">
-    {!! Form::hidden('id',$post->id) !!}
-    {!! Form::input('text','upPost',$post->post,['required','class'=>'form-control']) !!}
+  @section('content')
+  <div class="container new-post">
+    <h2 class="page-header">新規投稿</h2>
+    {!! Form::open(['url'=>'post/create'])!!}
+    <div class="form-group">
+      {!! Form::input('text','newPost',null,['class'=>'form-control','placeholder'=>'投稿内容（100文字以内）','align'=>'middle'])!!}
     @if(isset($errormessage))
     <p class="error">{{$errormessage}}</p>
     @endif
     @if(isset($emptymessage))
     <p class="error">{{$emptymessage}}</p>
     @endif
+      {!! Form::hidden('newName',Auth::user()->name)!!}
+
+    </div>
+    <button type="submit" class="btn post-button pull-right">投稿する</button>
+    {!! Form::close() !!}
   </div>
-  <button type="submit" class="btn btn-primary pull-right">更新</button>
-  {!! Form::close() !!}
-</div>
-@endsection
+ @endsection
